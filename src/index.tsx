276a6c5779490ad1c037da2a6b1ddb9e49b8e478
Maryia Miadzvedzeva <Maryia.Miadzvedzeva@missguided.com>
2022-09-25 16:59:57 +0200

    Integrate FE with new products service

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,6 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { theme } from "~/theme";
-import { worker } from "./mocks/browser";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -15,11 +14,10 @@ const queryClient = new QueryClient({
   },
 });
 
-// Display mocked data untill a real server is provided
-// if (import.meta.env.DEV) {
-  // const { worker } = await import("./mocks/browser");
+if (import.meta.env.DEV) {
+  const { worker } = await import("./mocks/browser");
   worker.start({ onUnhandledRequest: "bypass" });
-// }
+}
 
 const container = document.getElementById("app");
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
